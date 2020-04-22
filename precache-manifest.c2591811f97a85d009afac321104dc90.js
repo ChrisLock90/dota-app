845b10d42c2e29e8091cb97497c80c4b53846d0e
unknown <christopher_lock@outlook.com>
2020-04-22 12:02:57 +0100

    Updates

--- precache-manifest.c2591811f97a85d009afac321104dc90.js
+++ precache-manifest.c2591811f97a85d009afac321104dc90.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "84a3c15afacde7f38d983bf20256244d",
+    "revision": "c58e738fdf83b57b828bfa19f36d82ae",
     "url": "/dota-app/index.html"
   },
   {
-    "revision": "79587bd50f81ede56767",
+    "revision": "55706f795f8b427dec9b",
     "url": "/dota-app/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "c68653e785329397ade2",
-    "url": "/dota-app/static/js/2.b3779377.chunk.js"
+    "revision": "3ccce65d8c58d68c33c0",
+    "url": "/dota-app/static/js/2.ab97b5cd.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/dota-app/static/js/2.b3779377.chunk.js.LICENSE.txt"
+    "url": "/dota-app/static/js/2.ab97b5cd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "79587bd50f81ede56767",
-    "url": "/dota-app/static/js/main.c5cd6bc8.chunk.js"
+    "revision": "55706f795f8b427dec9b",
+    "url": "/dota-app/static/js/main.72975d26.chunk.js"
   },
   {
     "revision": "3af3605ae71cdd4d4dea",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dota-app/precache-manifest.d6d0d197e676e93bf92975ea77d3a658.js"
+  "/dota-app/precache-manifest.c2591811f97a85d009afac321104dc90.js"
 );
 
 self.addEventListener('message', (event) => {

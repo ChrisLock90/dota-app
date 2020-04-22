00eaff3f2038ed10ad4bf4e8ffca96539fc847b2
unknown <christopher_lock@outlook.com>
2020-04-22 11:59:53 +0100

    Connected to API and display a name

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import Techies from './Techies.jpg';
 import './App.css';
+import DotaApi from './Services/DotaApi';
 
-function App() {
-  return (
-    <div className="Dota App">
-      <header className="Dota 2 Techies Stats">
+class App extends React.Component {    
+  
+  render() {
+    return (
+      <div className="Dota App">
+        <div>
         <img src={Techies} className="Techies" alt="Techies" />
-        <p>
-          Dota 2 app to view Techies stats
-        </p>                        
-      </header>
-      <body>
-        
-      </body>
-    </div>
-  );
+          <p>
+            Dota 2 app to view Techies stats
+          </p>                             
+          <div><DotaApi /></div>          
+        </div>           
+      </div>        
+    );
+  }
 }
 
 export default App;

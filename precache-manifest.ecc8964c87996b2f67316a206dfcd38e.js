f85fbc5991c1131fbfafcc52be086c9892e78b17
unknown <christopher_lock@outlook.com>
2020-04-22 10:31:54 +0100

    Updates

---
+++ precache-manifest.ecc8964c87996b2f67316a206dfcd38e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6868f51693b0915b6f932bdbad16a6b8",
+    "url": "/dota-app/index.html"
+  },
+  {
+    "revision": "21df366221a2c4a85f69",
+    "url": "/dota-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c68653e785329397ade2",
+    "url": "/dota-app/static/js/2.b3779377.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dota-app/static/js/2.b3779377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21df366221a2c4a85f69",
+    "url": "/dota-app/static/js/main.03f09cd9.chunk.js"
+  },
+  {
+    "revision": "3af3605ae71cdd4d4dea",
+    "url": "/dota-app/static/js/runtime-main.ad1abd2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dota-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
